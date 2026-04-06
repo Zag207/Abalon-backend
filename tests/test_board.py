644d08db3d3f5fba22dc0f80299e7b8a3342d8c2
Zag207 <danilka.stepanov04@gmail.com>
2026-04-06 17:35:15 +0300

    check for linear tests

--- tests/test_board.py
+++ tests/test_board.py
@@ -140,3 +140,85 @@ class TestBoardGetCircleLine:
         circle_line = board.get_circle_line(circle_checked, MovingDirections.Right)
 
         assert circle_line == [circle_checked, edge_circle]
+
+
+class TestBoardCheckForLinear:
+    def test_check_for_linear_valid_capture_with_enemy_circles(self):
+        my_circle = Circle(CircleCoords(5, 5), CircleTeam.White)
+        my_circle_2 = Circle(CircleCoords(5, 6), CircleTeam.White)
+        enemy_circle = Circle(CircleCoords(5, 7), CircleTeam.Black)
+        circle_line = [my_circle, my_circle_2, enemy_circle]
+        board = Board(circle_line)
+
+        result = board.check_for_linear(circle_line, MovingDirections.Right, CircleTeam.White, CircleTeam.Black)
+
+        assert result is True
+
+    def test_check_for_linear_invalid_too_many_my_circles(self):
+        my_circles = [
+            Circle(CircleCoords(5, 5), CircleTeam.White),
+            Circle(CircleCoords(5, 6), CircleTeam.White),
+            Circle(CircleCoords(5, 7), CircleTeam.White),
+            Circle(CircleCoords(5, 8), CircleTeam.White),
+        ]
+        circle_line = my_circles
+        board = Board(circle_line)
+
+        result = board.check_for_linear(circle_line, MovingDirections.Right, CircleTeam.White, CircleTeam.Black)
+
+        assert result is False
+
+    def test_check_for_linear_invalid_too_many_enemy_circles(self):
+        my_circles = [
+            Circle(CircleCoords(5, 5), CircleTeam.White),
+            Circle(CircleCoords(5, 6), CircleTeam.White),
+        ]
+        enemy_circles = [
+            Circle(CircleCoords(5, 7), CircleTeam.Black),
+            Circle(CircleCoords(5, 8), CircleTeam.Black),
+        ]
+        circle_line = my_circles + enemy_circles
+        board = Board(circle_line)
+
+        result = board.check_for_linear(circle_line, MovingDirections.Right, CircleTeam.White, CircleTeam.Black)
+
+        assert result is False
+
+    def test_check_for_linear_invalid_enemy_not_consecutive(self):
+        my_circles = [
+            Circle(CircleCoords(5, 5), CircleTeam.White),
+            Circle(CircleCoords(5, 6), CircleTeam.White),
+        ]
+        enemy_circle_wrong_place = [
+            Circle(CircleCoords(5, 8), CircleTeam.Black),
+        ]
+        circle_line = my_circles + enemy_circle_wrong_place
+        board = Board(circle_line)
+
+        result = board.check_for_linear(circle_line, MovingDirections.Right, CircleTeam.White, CircleTeam.Black)
+
+        assert result is False
+
+    def test_check_for_linear_invalid_no_enemy_at_end_of_board(self):
+        my_circles = [
+            Circle(CircleCoords(1, 8), CircleTeam.White),
+            Circle(CircleCoords(1, 9), CircleTeam.White),
+        ]
+        circle_line = my_circles
+        board = Board(circle_line)
+
+        result = board.check_for_linear(circle_line, MovingDirections.Right, CircleTeam.White, CircleTeam.Black)
+
+        assert result is False
+
+    def test_check_for_linear_invalid_no_last_my_circle(self):
+        enemy_circles = [
+            Circle(CircleCoords(5, 5), CircleTeam.Black),
+            Circle(CircleCoords(5, 6), CircleTeam.Black),
+        ]
+        circle_line = enemy_circles
+        board = Board(circle_line)
+
+        result = board.check_for_linear(circle_line, MovingDirections.Right, CircleTeam.White, CircleTeam.Black)
+
+        assert result is False
