8850ad6f7ac22d00c5d3e9ed424bac0b5458d0f7
Zag207 <danilka.stepanov04@gmail.com>
2026-04-06 13:11:59 +0300

    test_get_new_coords

--- tests/test_coords.py
+++ tests/test_coords.py
@@ -2,6 +2,7 @@ import pytest
 
 from core.delta_coords import DeltaCoords
 from core.moving_directions import MovingDirections
+from core.circle_coords import CircleCoords
 
 
 class TestCoords:
@@ -21,3 +22,11 @@ class TestCoords:
         delta_coords = DeltaCoords.get_delta_coords_from_moving(direction)
         assert delta_coords.delta_line == expected_line
         assert delta_coords.delta_diagonal == expected_diagonal
+    
+    def test_get_new_coords(self):
+        delta_coords = DeltaCoords.get_delta_coords_from_moving(MovingDirections.UpRight)
+        coords = CircleCoords(5, 6)
+        new_coords = coords.get_new_coords(delta_coords)
+
+        assert(new_coords.line == 4)
+        assert(new_coords.diagonal == 7)
