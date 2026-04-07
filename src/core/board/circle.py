e4a0520c7d6bc69c0267354fd99d2cf7297baf45
Zag207 <danilka.stepanov04@gmail.com>
2026-04-07 18:54:02 +0300

    remove is_checked and is_moving

--- src/core/board/circle.py
+++ src/core/board/circle.py
@@ -7,21 +7,15 @@ from core.geometry.delta_coords import DeltaCoords
 class Circle:
     circle_id: uuid.UUID
     coords: CircleCoords
-    is_checked: bool
-    is_moving: bool
     circle_type: CircleTeam
 
     def __init__(
             self,
             coords: CircleCoords,
-            circle_type: CircleTeam,
-            is_checked: bool = False,
-            is_moving: bool = False
+            circle_type: CircleTeam
             ) -> None:
         self.coords = coords
         self.circle_id = uuid.uuid6()
-        self.is_checked = is_checked
-        self.is_moving = is_moving
         self.circle_type = circle_type
 
     def update_coords(self, deltaCoords: DeltaCoords) -> None:
